41843b409a163d93631b743f42ea0880e3a274cb
Nikolai Evtushenko <109798979+ewq-doublecast@users.noreply.github.com>
2024-01-17 14:25:08 +0300

    Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace TemplateMethod
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Назначение: шаблонный метод определяет основу алгоритма и позволяет подклас-сам переопределять некоторые шаги алгоритма,
+        /// не изменяя его структуры в целом.
+        /// Другими словами: шаблонный метод — это каркас, в который наследники могут подставить реализации недостающих элементов.
+        /// </summary>
+        
+        public abstract class AbstractClass
+        {
+            public void TemplateMethod()
+            {
+                OperationOne();
+                OperationTwo();
+            }
+
+            public abstract void OperationOne();
+
+            public abstract void OperationTwo();
+        }
+
+        public class ConcreteClassOne : AbstractClass
+        {
+            public override void OperationOne()
+            {
+                Console.WriteLine("1-я операция 1-го конкретного класса");
+            }
+
+            public override void OperationTwo()
+            {
+                Console.WriteLine("2-я операция 1-го конкретного класса");
+            }
+        }
+
+        public class ConcreteClassTwo : AbstractClass
+        {
+            public override void OperationOne()
+            {
+                Console.WriteLine("1-я операция 2-го конкретного класса");
+            }
+
+            public override void OperationTwo()
+            {
+                Console.WriteLine("2-я операция 2-го конкретного класса");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ConcreteClassOne concreteClassOne = new ConcreteClassOne();
+            ConcreteClassTwo concreteClassTwo = new ConcreteClassTwo();
+
+            concreteClassOne.TemplateMethod();
+            concreteClassTwo.TemplateMethod();
+        }
+    }
+}
